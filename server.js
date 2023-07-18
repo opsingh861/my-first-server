27fbb5cc3f9d5880973482459fa1c8d590264a4c
opsingh861 <62853140+opsingh861@users.noreply.github.com>
2023-07-18 10:22:37 +0530

    Created first server

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require("http")
+
+const port = 8081
+
+http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" })
+    response.write("<h1>Hello, Bholu</h1>")
+    response.end();
+}).listen(port, () => {
+    console.log(`Nodejs server started on port ${port}`)
+});
+
+
+// http:localhost:8081
