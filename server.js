c41e3853823e20de84d8a0791f1682147fcbd6a4
opsingh861 <62853140+opsingh861@users.noreply.github.com>
2023-07-18 20:26:01 +0530

    DELETE method done

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require("http")
 
 const port = 8081
+let body = ""
 
 const todoList = ["Complete Node Byte", "Play Cricket"]
-let body = ""
 
 http.createServer((request, response) => {
     const { method, url } = request;
@@ -17,11 +17,35 @@ http.createServer((request, response) => {
             request.on("error", (err) => {
                 console.log(err)
             }).on("data", (chunk) => {
+                let body = ""
                 body += chunk;
                 // console.log(chunk)
             }).on("end", () => {
                 body = JSON.parse(body);
-                console.log("data: ", body)
+                // console.log("data: ", body)
+                let newToDo = todoList;
+                newToDo.push(body.Item)
+                console.log(newToDo)
+                response.writeHead(201)
+            })
+        } else if (method === "DELETE") {
+            // let body = "";
+            request.on("error", (err) => {
+                console.log(err)
+            }).on("data", (chunk) => { 
+                body += chunk;
+
+            }).on("end", () => {
+                body = JSON.parse(body);
+                let deleteThis = body.Item;
+
+                for (let i = 0; i < todoList.length; i++) {
+                    if (todoList[i] === deleteThis) {
+                        todoList.splice(i, 1)
+                        break;
+                    }
+                }
+                console.log(todoList)
             })
         }
     }
@@ -32,4 +56,4 @@ http.createServer((request, response) => {
 });
 
 
-// http:localhost:8081
+// http:localhost:8081 
